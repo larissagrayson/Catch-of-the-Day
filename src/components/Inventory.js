c91e587614e4a709f033a0da14fb2526e78256c0
Larissa Grayson <59101865+larissagrayson@users.noreply.github.com>
2021-01-02 13:05:19 -0500

    Removed console logs

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -33,7 +33,6 @@ class Inventory extends React.Component {
     authHandler = async (authData) => {
         // 1. Look up the current store in the firebase database
         const store = await base.fetch(this.props.storeId, { context: this });
-        console.log(store);
         // 2. Claim it if there is no owner
         if(!store.owner) {
             // save it as our own
@@ -46,7 +45,6 @@ class Inventory extends React.Component {
             uid: authData.user.uid,
             owner: store.owner || authData.user.uid
         });
-        console.log(authData);
     };
 
     authenticate = (provider) => {
@@ -62,7 +60,6 @@ class Inventory extends React.Component {
     };
 
     logout = async () => {
-        console.log('Logging out!');
         await firebase.auth().signOut();
         this.setState({ uid: null });
     }
